c947f0f8aedaeef78f772c1136a1766790094305
lazarevFedor <lazarevfedor@gmail.com>
2024-04-07 21:18:59 +0300

    start

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <Windows.h>
+#include <limits>
+#undef max
+using namespace std;
+
+
+void clearStream() {
+    cin.clear();
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    cin.sync();
+}
+
+
+void mainMenu(){
+    cout << "1) Сформировать дерево\n" <<
+    "2) Вывести дерево\n" <<
+    "3) Действия с деревом\n-->> ";
+}
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    SetConsoleOutputCP(CP_UTF8);
+
     return 0;
 }
